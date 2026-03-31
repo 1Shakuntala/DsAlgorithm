819d6c8fa76e91c06ef7b5c608d3e18f0d30c2d8
Shakuntala <shaku.india@gmail.com>
2026-03-31 23:05:37 +0530

    commit

---
+++ src/BinarySearch/Nth_Root_of_a_Number_using_Binary_Search.java
@@ -0,0 +1,30 @@
+package BinarySearch;
+
+public class Nth_Root_of_a_Number_using_Binary_Search {
+
+    // Function directly here (no inner class needed)
+    public static int nthRoot(int n, int m) {
+        int low = 1, high = m;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            long ans = 1;
+            for (int i = 0; i < n; i++) {
+                ans *= mid;
+                if (ans > m) break;
+            }
+
+            if (ans == m) return mid;
+            if (ans < m) low = mid + 1;
+            else high = mid - 1;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int result = nthRoot(3, 64);
+        System.out.println(result);
+    }
+}
